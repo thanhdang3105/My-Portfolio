81a27deb23973e51fd014016c8fb3a74a007fd02
ThanhDang3105 <thanhls1235ls12@gmail.com>
2022-09-16 12:52:25 +0700

    update

--- src/components/BodyContent/Welcome/index.js
+++ src/components/BodyContent/Welcome/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from '../BodyContent.module.scss';
 import className from 'classnames/bind';
-
 const cx = className.bind(styles)
 
 export default function Welcome() {
@@ -9,7 +8,7 @@ export default function Welcome() {
     <div id="Welcome" className={cx('welcome_wrapper')}>
         <div className={cx('content')}>
             <h1 className={cx('welcome_title')}>Chào mừng đến với Portfolio của tôi</h1>
-            <p className={cx('welcome_desc')}>Xin chào! Tôi tên Thanh. Cảm ơn bạn đã ghé qua Portfolio của tôi. Mời bạn xem xét xung quanh và hy vọng bạn có thể cho tôi một số phản hồi. Cảm ơn bạn rất nhiều!</p>
+            <p className={cx('welcome_desc')}>Xin chào! Tôi tên Thanh. Cảm ơn bạn đã ghé qua Portfolio của tôi. Hy vọng bạn sẽ xem và có thể cho tôi một số phản hồi. Cảm ơn bạn rất nhiều!</p>
             <a className={cx('btn_downCV')} href="https://www.topcv.vn/download-cv?cv_id=DgcGA1YHUlBaAlMGBABWUwwFAFJWUAUEA1ACBg45c2&dontcount=1">Tải CV</a>
         </div>
     </div>
